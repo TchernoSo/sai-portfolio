6dd27b758e0a9d27a6e2c924a833521d71f44a3e
Tcherno So <tcherno.so@hotmail.com>
2024-07-10 10:47:39 +0100

    Last updates

--- scripts.js
+++ scripts.js
@@ -1,10 +1,10 @@
 // scripts.js
 
-// scripts.js
-
 document.addEventListener('DOMContentLoaded', function () {
     var navbarToggler = document.querySelector('.navbar-toggler');
     navbarToggler.addEventListener('click', function () {
         this.classList.toggle('collapsed');
     });
 });
+
+
